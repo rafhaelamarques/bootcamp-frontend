24071582502ddb1eba504fa6cef28834103b09e8
Rafhaela Marques <rafaelamarques_z@hotmail.com>
2022-01-30 01:00:57 -0300

    todo list

---
+++ javascript/assets/scripts-todo.js
@@ -0,0 +1,27 @@
+function addTask() {
+    var list = document.getElementById("list");
+    var item = document.createElement("li");
+    var task = document.createElement("span");
+    var checkbox = document.createElement("input");
+    item.setAttribute("class", "unchecked");
+    task = document.getElementById("task").value;
+    checkbox.type = "checkbox";
+    if (document.getElementById("task").value === '') {
+        alert("Insira alguma tarefa!");
+    } else {
+        item.appendChild(checkbox);
+        item.appendChild(document.createTextNode(task));
+        list.appendChild(item);
+        document.getElementById("task").value = '';
+    }
+    item.addEventListener('click', checked);
+}
+
+function checked() {
+    this.className = "checked";
+}
+
+
+function unchecked() {
+    this.className = "unchecked";
+}
