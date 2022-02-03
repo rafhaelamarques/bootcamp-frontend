6354fedd007af73229b41f8213137fd54b6845c5
Rafhaela Marques <rafaelamarques_z@hotmail.com>
2022-02-02 23:16:51 -0300

    update

--- javascript/assets/scripts-justcats.js
+++ javascript/assets/scripts-justcats.js
@@ -1,23 +1,23 @@
-const BASE_URL = 'https://thatcopy.pw/catapi/rest/';
-// const BASE_URL = 'https://api.thecatapi.com/v1/images/search';
+// const BASE_URL = 'https://thatcopy.pw/catapi/rest/';
+const BASE_URL = 'https://api.thecatapi.com/v1/images/search';
 
-const getCat = async() => {
+loadImg();
+
+getCat = async() => {
     try {
-        const data = await fetch(BASE_URL);
-        const json = await data.json();
-        return json.webpurl;
+        data = await fetch(BASE_URL);
+        json = await data.json();
+        return json.url;
     } catch (e) {
         console.log(e.message);
     }
 };
 
 
-const loadImg = async() => {
-    const catImg = document.getElementById('img')[0];
+loadImg = async() => {
+    catImg = document.getElementById('img');
     catImg.src = await getCat();
 };
-
-loadImg();
 
 var catBtn = document.getElementById('btn');
 catBtn.addEventListener('click', loadImg);
