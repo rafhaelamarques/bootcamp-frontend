d9c958b2b637d2b250e69f136cccb495ba24ff1b
Rafhaela Marques <rafaelamarques_z@hotmail.com>
2022-02-19 17:54:00 -0300

    update

--- javascript/assets/desafio.js
+++ javascript/assets/desafio.js
@@ -29,3 +29,28 @@ console.log(maxDispositivos(line));
 
 //desafio 5
 console.log(line[0] * line[1]);
+
+//desafio 6
+let lines = gets().split("\n");
+
+var l = parseInt(lines[0]);
+var c = parseInt(lines[1]);
+
+if (l % 2 === 0)
+  if (c % 2 === 0) print(1);
+  else print(0);
+else if (l % 2 != 0) print(1);
+ else print(0);      
+
+
+//desafio 7
+
+const [precoAlcool, precoGasolina, rendimentoAlcool, rendimentoGasolina] = 
+  lines[0].split(' ').map(x => parseFloat(x));
+
+const precoPorKmAlcool = rendimentoAlcool * precoAlcool;
+const precoPorKmGasolina = rendimentoGasolina * precoGasolina;
+
+const maisEconomico = Math.round(precoPorKmGasolina) <= Math.round(precoPorKmAlcool) ? 'G' : 'A';
+
+print(maisEconomico);
